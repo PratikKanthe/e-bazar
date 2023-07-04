81cd578b4afc5b70b1494e1e79389062d20d7b0e
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-07-04 23:44:40 +0530

    add pages

---
+++ app/pages/product.tsx
@@ -0,0 +1,7 @@
+const Product = () => {
+    return <>
+     <h1> Explore Amazing <span style={{color: "blue", fontSize: "45", fontStyle: "italic", fontFamily: "sans-serif"}}>Products</span>  </h1>
+    </>
+  }
+  
+  export default Product
---
+++ app/profile.tsx
@@ -0,0 +1,7 @@
+const Profile = () => {
+    return <>
+     <h1> Welcome to the <span style={{color: "blue"}}>Profile Page</span>  </h1>
+    </>
+  }
+  
+  export default Profile
